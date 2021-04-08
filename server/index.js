7f031717bf1cd3aeeffb26bf15069c05e3109cec
Mark <amwhow@gmail.com>
2021-04-08 17:10:51 +1000

    comment modification

--- server/index.js
+++ server/index.js
@@ -6,15 +6,13 @@ import postRoutes from './routes/posts.js'
 // initialise the app
 const app = express();
 
-// this adds the prefix '/posts' behind all routes in post.js, equal to the 'routes.rb' file regarding functionality
-
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+// this adds the prefix '/posts' behind all routes in post.js, equal to the 'routes.rb' file regarding functionality
 app.use('/posts', postRoutes);
 
 // mongo section, credentials will be secured later
-
 const CONNECTION_URL = 'mongodb+srv://amwhow33:amwhow3305@cluster0.8oqr7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 3500;
 
