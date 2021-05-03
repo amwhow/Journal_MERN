fb6c4dcaeefdc6a4b7e1fa3cc3f7c890083d364d
Mark <amwhow@gmail.com>
2021-05-03 11:25:36 +1000

    implemented front end sign in/sign up form

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // const url = "https://journal-project-mern.herokuapp.com/posts";
 const url = "http://localhost:3500/posts";
 
-
 // axios integrated the two steps of fetching data and .json() it
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost);
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,109 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Input from "./Input";
+import useStyles from "./styles";
 
 const Auth = () => {
-  const state = null
-  return (
-    <div>
-      AUTH
-    </div>
-  )
-}
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [signedUp, setSignedUp] = useState(false);
 
-export default Auth
+  // toggle function, set showPassword to true if it was false and vice versa
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = () => {
+    return 0;
+  };
+
+  const handleChange = () => {
+    return 0;
+  };
+
+  const switchMode = () => {
+    setSignedUp((prevSignedUp) => !prevSignedUp);
+    setShowPassword(false);
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{signedUp ? "Sign Up" : "Sign In"}</Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {signedUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email"
+              handleChange={handleChange}
+              half
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              half
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {signedUp && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {signedUp ? "Sign up" : "Sign in"}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                { signedUp ? 'Already have an account? Sign in here' : "Don't have an account yet? Sign up here"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
