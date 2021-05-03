7858d20565616ce7cfbaa09df24a7330b4e68c4c
Mark <amwhow@gmail.com>
2021-05-03 11:25:09 +1000

    customised component for form inputs

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  TextField,
+  Grid,
+  InputAdornment,
+  IconButton,
+} from "@material-ui/core";
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const Input = ({ name, handleChange, label, half, type, autoFocus, handleShowPassword }) => {
+  return (
+    <Grid item xs={half ? 6 : 12} md={12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password" && {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleShowPassword}
+                >
+                  {type === "password" ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
